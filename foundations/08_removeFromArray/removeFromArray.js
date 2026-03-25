51375140cb8b4102c4082179c6a36711d088427e
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-25 18:36:46 -0500

    removeFromArray
    function removes specified items from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...valuesToRemove) {
+let remainingValue = [];
+for (let num of arr) {
+    if (valuesToRemove.includes(num)){
+        continue;
+    }else {
+        remainingValue.push(num);
+    }
+}
+return remainingValue;
 };
 
 // Do not edit below this line
