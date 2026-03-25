8bf382a3401404267a2126c2633552287530d491
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-24 19:17:31 -0500

    repeatString
    wrote a function that takes a string and a multiplier and returns
    the string repeated multiplier number of times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(word,multiplier) {
+let newWord = '';
+// Check if multiplier is valid
+if (multiplier < 0) {
+    return 'ERROR';
+}
+//loop multiplier amount of times
+let i = 0;
+while (i < multiplier) {
+     // add word to word
+    newWord = newWord + word;
+    i++;
+}
+   
+//return string
+return newWord;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
