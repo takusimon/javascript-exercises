36a87d4b6e7e4167f5a38ea677ce1bc9994d3dc1
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-25 19:27:43 -0500

    sumAll
    working with large numbers and with larger number first

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(smallestNum,biggestNum) {
+if (smallestNum > biggestNum){
+    [biggestNum,smallestNum] = [smallestNum,biggestNum];
+}
+let n = (biggestNum - smallestNum) + 1;
+let sum = (n/2) * (smallestNum + biggestNum);
+return sum;
 };
 
 // Do not edit below this line
