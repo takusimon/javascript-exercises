6abcf8c327eb1d2d1cafd587d24f62e907ebcf62
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-26 16:16:55 -0500

    leapYears
    wrote a function function that determines whether
    or not a given year is a leap year.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+if (year % 100 == 0) {
+    // if it's divisible by 100 it must also be divisible by 400 to be a leap
+    // year
+    if (year % 400 == 0) {
+        return true;
+    }else {
+        return false;
+    }
+} else if (year % 4 == 0) {
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
