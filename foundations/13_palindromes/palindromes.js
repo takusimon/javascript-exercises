dbe6bf3c2e05aca39e9def8db74e45d3b9feda38
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-04-04 10:10:27 -0500

    Palindrome
    created a function that tests whether a given word is a palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+function removePunctuation(cleanedWord){
+	return cleanedWord.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+}
+const palindromes = function(word) {
+	const nopunctuation = removePunctuation(word);
+	const cleanedWord = nopunctuation.replaceAll(" ", "").toLowerCase();
+	return cleanedWord === cleanedWord.split("").reverse().join("") ? true : false;
 };
 
 // Do not edit below this line
