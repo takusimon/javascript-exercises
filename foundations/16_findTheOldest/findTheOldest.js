7aadd59a99bbcf0b42cf208a98750a89b54400b8
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-04-04 15:25:04 -0500

    given a list of people objects function returns the oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	return people.reduce((oldest, current) => {
+		const oldestAge = (oldest.yearOfDeath ?? new Date().getFullYear()) - oldest.yearOfBirth;
+		const currentAge = (current.yearOfDeath ?? new Date().getFullYear()) - current.yearOfBirth;
 
+		return currentAge > oldestAge ? current : oldest;
+	});
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
