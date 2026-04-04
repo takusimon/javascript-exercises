17ed253b097c16dbe07c010114c4537bd3fa9248
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-04-04 12:10:16 -0500

    Fibonacci
    given a term n Fibonacci() returns the attended term in the fibonacci
    sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+	//instead of generating the sequnce just find the nth term in the sequence
+	// use the the fibonacci formula
+	if (term < 0) {
+		return "OOPS";
+	} else if (Number(term) != NaN) {
+		term = Number(term);
+		const phi = (1 + Math.sqrt(5)) / 2;
+		const psi = (1 - Math.sqrt(5)) / 2;
+		return Math.round((phi ** term - psi ** term) / Math.sqrt(5));
+	}
 };
 
 // Do not edit below this line
